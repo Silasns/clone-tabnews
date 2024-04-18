9cf9e2d3046bced78264c08d4b0d4745058f2c33
Silas Cardoso <43625566+Silasns@users.noreply.github.com>
2024-04-18 23:00:42 +0000

    Adicionando texto

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e4a85d9a6bd8bf21"; }
+/******/ 		__webpack_require__.h = function() { return "e50a4205c34c8524"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
