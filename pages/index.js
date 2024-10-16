c75bcdb1cbe498f1f77edebd5d3fb372c28ea5ac
Silas Cardoso <43625566+Silasns@users.noreply.github.com>
2024-10-16 21:40:27 +0000

    Adicionando gitignore

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Lazaro love u my sol, se love me de um risadin!!</h1>
+    return <h1>Lazaro love u my sol, se love me de um risadin!!(Como sempre lembro de tu)</h1>
 }
 
 export default Home;
