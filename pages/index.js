c91339087eac0f8e09129cbe27b4623e31ae0d3e
Silas Cardoso <43625566+Silasns@users.noreply.github.com>
2024-10-25 18:12:41 +0000

    Inclusão das prioridades

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,41 @@
 function Home() {
   return (
-    <h1>
-      Lazaro love u my sol, se love me de um risadin!!(Como sempre lembro de tu)
-    </h1>
+    <div>
+      <p>
+        O domínio ta com meu nome pq pensei em a principio utilizar como meu
+        portifólio, mas nesse meio tempo de esperar o pagamento do domínio,
+        acabei tendo uma ideia o:
+      </p>
+      <h1>casarfacil</h1>
+      <h3>
+        Como estou nesse processo de noivar/casar e fuçando alguns sites que vc
+        pode subir sua lista de compras muitos deles cobram taxas e valores por
+        esse serviço, então tive a ideia de criar meu próprio mas que
+        futuramente penso em disponibilizar para que outras pessoas possam usar
+        tambem basicamente oque pensei a principio em ter no site é:
+      </h3>
+      <ul>
+        <li>Loja de presentes para o casal</li>
+        <li>
+          Área estilo o tabnews para se tratar como se foi um forum com dicas,
+          sugestões e discussões de quem ja casou
+        </li>
+        <li>Quadro kabam para o casal se organizar</li>
+        <li>Lista de convidados para os noivos</li>
+        <li>Confirmação de presença para os convidados</li>
+        <li>Quadro de mensagens para o casal</li>
+        <li>
+          Envio do local da festa, somente para quem confirmou a presença e ou
+          esta na lista de convidado que devem ser convidados para este momento
+        </li>
+        <li>
+          Ter um vídeo curto ou uma foto como agradecimento na parte final
+          depois da compra de um presente
+        </li>
+      </ul>
+
+      <p>A principio é isso, hehe</p>
+    </div>
   );
 }
 
