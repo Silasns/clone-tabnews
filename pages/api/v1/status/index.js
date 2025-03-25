f7ee5a285c99dacfe3aa840e8b4e482317d78334
Silas Cardoso <43625566+Silasns@users.noreply.github.com>
2025-03-25 23:42:45 +0000

    create return database-status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,24 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as SUM;");
-  console.log(result.rows);
-  response.status(200).json({ texto: "Tudo certo!" });
+  const updateAt = new Date().toISOString();
+  const dataBaseVersion = await database.query("SELECT version()");
+  const dataBaseMaxConnections = await database.query("SHOW max_connections;");
+  const dataBaseConnectionsUsed = await database.query(
+    "SELECT count(*) FROM pg_stat_activity;",
+  );
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        max_connections: dataBaseMaxConnections.rows.find((m) => m)
+          .max_connections,
+        opened_connections: dataBaseConnectionsUsed.rows.find((c) => c).count,
+        version: dataBaseVersion.rows.find((v) => v).version,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,4 +2,23 @@ test("GET to api/v1/status should return 200", async () => {
   // porem await so pode esperar em uma função assincrona
   const response = await fetch("http://localhost:3000/api/v1/status"); //await fica esperando, esperar o retorno do fetch
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toBe(parsedUpdateAt);
+
+  const versionDataBase =
+    "PostgreSQL 17.4 on x86_64-pc-linux-musl, compiled by gcc (Alpine 14.2.0) 14.2.0, 64-bit";
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  expect(responseBody.dependencies.database.version).toBe(versionDataBase);
+
+  const maxConnectionsDataBase = "100";
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  expect(responseBody.dependencies.database.max_connections).toBe(
+    maxConnectionsDataBase,
+  );
+
+  expect(responseBody.dependencies.database.opened_connections).toBeTruthy();
 });
