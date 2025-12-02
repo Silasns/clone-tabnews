185ea030b8458953f26280d9f8ee775037d4e712
Silas Cardoso <43625566+Silasns@users.noreply.github.com>
2025-12-02 01:10:21 +0000

    Correção endpoint migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -36,17 +36,6 @@ export default async function migrations(request, response) {
     return response.status(200).json(pendingMigrations);
   }
 
-  if (request.method === "PUT") {
-    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
-  }
-
-  if (request.method === "PATCH") {
-    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
-  }
-
+  await dbClient.end();
   return response.status(405).end();
 }
